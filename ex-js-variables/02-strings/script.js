4cfda250740d8a93bf75312458a36c2a6b9fadd8
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-01-08 13:15:07 +0100

    add: first script and half the second

---
+++ ex-js-variables/02-strings/script.js
@@ -0,0 +1,32 @@
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata name e assegnale il tuo nome.
+// Stampa il valore di nome nella console.
+
+let Name = "Sebastiano";
+console.log(Name)
+
+// 2. Tipi primitivi
+// Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+// un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
+
+let number = 5;
+let text= "Golgo";
+let bool= 1;
+console.log(number,text,bool)
+
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il nome completo
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName); // Output: "Jane Smith"
+
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = null;
+console.log(numberOfLettersInAlphabet); // Output: 26
